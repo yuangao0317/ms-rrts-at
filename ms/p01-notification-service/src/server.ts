3e33ab59b1d78174b094b3a44f6dc9f755ebd824
YuanGao0317 <yuangao0317@gmail.com>
2024-05-05 20:17:22 -0400

    add httpServer to p01-notification

---
+++ ms/p01-notification-service/src/server.ts
@@ -0,0 +1,28 @@
+import 'express-async-errors';
+import http from 'http';
+
+import { Logger } from 'winston';
+import { config } from '@notifications/config';
+import { winstonLogger } from '@yuangao0317/ms-rrts-at-shared-common';
+import { Application } from 'express';
+
+const SERVER_PORT = 4001;
+const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'notificationServer', 'debug');
+
+export function start(app: Application): void {
+    startHttpServer(app);
+}
+
+function startHttpServer(app: Application): void {
+  try {
+    const httpServer: http.Server = new http.Server(app);
+    log.info(`Worker with process id of ${process.pid} on notification server has started`);
+    httpServer.listen(SERVER_PORT, () => {
+      log.info(`Notification server running on port ${SERVER_PORT}`);
+    });
+  } catch (error) {
+    log.log('error', 'NotificationService startServer() method:', error);
+  }
+}
+
+
