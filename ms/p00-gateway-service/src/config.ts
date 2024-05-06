78b8b0082c5571f890906d0edf3b967bcde5ae7a
YuanGao0317 <yuangao0317@gmail.com>
2024-05-06 16:12:50 -0400

    change config for p00 and p01 to add ELASTIC_APM

--- ms/p00-gateway-service/src/config.ts
+++ ms/p00-gateway-service/src/config.ts
@@ -6,17 +6,37 @@ if (process.env.NODE_ENV === 'development') {
   dotenv.config({});
 }
 
+if (process.env.ENABLE_APM === '1') {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require('elastic-apm-node').start({
+    serviceName: 'ms-rrts-at-gateway-service',
+    serverUrl: process.env.ELASTIC_APM_SERVER_URL,
+    secretToken: process.env.ELASTIC_APM_SECRET_TOKEN,
+    environment: process.env.NODE_ENV,
+    active: true,
+    captureBody: 'all',
+    errorOnAbortedRequests: true,
+    captureErrorLogStackTraces: 'always'
+  });
+}
+
 class Config {
   public NODE_ENV: string | undefined;
+  public GATEWAY_JWT_TOKEN: string | undefined;
+  public JWT_TOKEN: string | undefined;
   public CLIENT_URL: string | undefined;
-  public RABBITMQ_ENDPOINT: string | undefined;
   public ELASTIC_SEARCH_URL: string | undefined;
+  public REDIS_HOST: string | undefined;
+  public AUTH_BASE_URL: string | undefined;
 
   constructor() {
     this.NODE_ENV = process.env.NODE_ENV || '';
+    this.GATEWAY_JWT_TOKEN = process.env.GATEWAY_JWT_TOKEN || '1234';
+    this.JWT_TOKEN = process.env.JWT_TOKEN || '1234';
     this.CLIENT_URL = process.env.CLIENT_URL || '';
-    this.RABBITMQ_ENDPOINT = process.env.RABBITMQ_ENDPOINT || '';
     this.ELASTIC_SEARCH_URL = process.env.ELASTIC_SEARCH_URL || '';
+    this.REDIS_HOST = process.env.REDIS_HOST || '';
+    this.AUTH_BASE_URL = process.env.AUTH_BASE_URL || '';
   }
 }
 
--- ms/p01-notification-service/src/config.ts
+++ ms/p01-notification-service/src/config.ts
@@ -6,6 +6,20 @@ if (process.env.NODE_ENV === 'development') {
   dotenv.config({});
 }
 
+if (process.env.ENABLE_APM === '1') {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require('elastic-apm-node').start({
+    serviceName: 'ms-rrts-at-notification-service',
+    serverUrl: process.env.ELASTIC_APM_SERVER_URL,
+    secretToken: process.env.ELASTIC_APM_SECRET_TOKEN,
+    environment: process.env.NODE_ENV,
+    active: true,
+    captureBody: 'all',
+    errorOnAbortedRequests: true,
+    captureErrorLogStackTraces: 'always'
+  });
+}
+
 class Config {
   public NODE_ENV: string | undefined;
   public CLIENT_URL: string | undefined;
