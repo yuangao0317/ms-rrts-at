b39a0257607416dc15ec98230f43f1e028f0c394
YuanGao0317 <yuangao0317@gmail.com>
2024-05-07 23:42:07 -0400

    add elasticsearch to p02-auth

--- ms/p02-auth-service/src/database.ts
+++ ms/p02-auth-service/src/database.ts
@@ -7,21 +7,19 @@ const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'authServer', 
 
 // https://sequelize.org/api/v6/class/src/sequelize.js~sequelize#instance-constructor-constructor
 export const sequelize: Sequelize = new Sequelize(config.MYSQL_DB!, {
-    dialect: 'mysql',
-    logging: false,
-    dialectOptions: {
-        multipleStatements: true
-    }
+  dialect: 'mysql',
+  logging: false,
+  dialectOptions: {
+    multipleStatements: true
+  }
 });
 
 export async function databaseConnection(): Promise<void> {
-    try {
-        await sequelize.authenticate();
-        log.info('AuthService - Mysql database connection has been established successfully.');
-    } catch (error) {
-        log.error('AuthService - Unable to connect to database.');
-        log.log('error', 'AuthService databaseConnection() method error:', error);
-    }
+  try {
+    await sequelize.authenticate();
+    log.info('AuthService - Mysql database connection has been established successfully.');
+  } catch (error) {
+    log.error('AuthService - Unable to connect to database.');
+    log.log('error', 'AuthService databaseConnection() method error:', error);
+  }
 }
-
-
