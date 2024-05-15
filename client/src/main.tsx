dffb9b5d4e70c67854b9a7dfa0bd7398f3c1715f
YuanGao0317 <yuangao0317@gmail.com>
2024-05-15 17:47:52 -0400

    add store to app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,11 +2,20 @@ import 'src/index.scss';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { Provider } from 'react-redux';
+import { Persistor, persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from 'src/App.tsx';
+import { store } from 'src/store/store';
+
+const persistor: Persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
